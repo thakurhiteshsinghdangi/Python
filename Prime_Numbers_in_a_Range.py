4f3dc883b0ccb6f47fbc50f4106ce0589001be95
Hitesh Dangi <hiteshsinghdangi11@gmail.com>
2026-06-15 11:49:15 +0530

    Implement prime number finder in a given range
    Add functionality to input a range and print prime numbers.

--- Prime_Numbers_in_a_Range.py
+++ Prime_Numbers_in_a_Range.py
@@ -1,1 +1,14 @@
+start = int(input("Enter start: "))
+end = int(input("Enter end: "))
 
+for num in range(start, end + 1):
+    if num > 1:
+        is_prime = True
+
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
+                is_prime = False
+                break
+
+        if is_prime:
+            print(num, end=" ")
