cbcea50bfc1d88487f0cc19c0d41fe0546e2f874
Hitesh Dangi <hiteshsinghdangi11@gmail.com>
2026-06-15 11:44:36 +0530

    Update Armstrong_Number.py

--- Armstrong_Number.py
+++ Armstrong_Number.py
@@ -1,1 +1,15 @@
+num = int(input("Enter a number: "))
 
+power = len(str(num))
+temp = num
+total = 0
+
+while temp > 0:
+    digit = temp % 10
+    total += digit ** power
+    temp //= 10
+
+if total == num:
+    print("Armstrong number")
+else:
+    print("Not an Armstrong number")
